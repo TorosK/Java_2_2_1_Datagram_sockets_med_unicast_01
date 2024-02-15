c287eb560d15880c741effae0aafdd49da67c775
Toros <toros.kutlu@claraconsulting.se>
2024-02-15 08:33:32 +0100

    javadoc + readme

--- Readme.md
+++ Readme.md
@@ -2,6 +2,10 @@ Console command to compile all java files in a directory:
 
 javac *.java
 
+For javadoc execution into 'doc' folder:
+
+javadoc -d doc *.java
+
 Open a terminal in directory and execute:
 
 java -jar Toros_Draw.jar 2000 localhost 2001
---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Draw","l":"Draw(int, String, int)","u":"%3Cinit%3E(int,java.lang.String,int)"},{"p":"<Unnamed>","c":"Draw","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"Draw","l":"sendPoint(Point)","u":"sendPoint(java.awt.Point)"}];updateSearchResults();
